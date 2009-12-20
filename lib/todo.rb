006c8a368c2d65393e6146a3673d1b69c47f3f0e
Wael Nasreddine <wael.nasreddine@gmail.org>
2009-12-20 04:03:28 +0100

    Add a function to read configuration from config_file.

--- lib/todo.rb
+++ lib/todo.rb
@@ -41,6 +41,22 @@ require 'todo/task'
 
 module Todo
 
+  def self.configuration
+	config_file = File.join(ENV['HOME'], *%w[.config todo config.yml])
+
+	begin
+	  config = YAML.load_file(config_file)
+	  raise "Invalid configuration - #{config_file}" unless config.is_a?(Hash)
+	  STDOUT.puts "Configuration loaded from #{config_file}"
+	rescue => err
+	  STDERR.puts "Configuration cannot be read from the config file, perhaps I couldn't parse it."
+	  STDERR.puts "\t" + err.to_s
+	  exit 1
+	end
+
+	config
+  end
+
   def self.version
 	yml = YAML.load(File.read(File.join(File.dirname(__FILE__), *%w[.. VERSION.yml])))
 	"#{yml[:major]}.#{yml[:minor]}.#{yml[:patch]}"
